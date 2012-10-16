a93f07e94273cbd50162234bd1b603f5415e84fe
Robert Onslow <ronslow@xlegal.co.uk>
2012-10-16 17:01:30 +0100

    Main commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject exchange "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-oauth "1.2.10"]
+                 [net.intensivesystems/conduit "0.8.1"]
+                 [org.clojars.kjw/commons-io "1.4"]
+                 ])
