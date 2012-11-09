15e715a0727771c52aecdaa576e6fb4a54b043b1
unknown <Robert Onslow@.(none)>
2012-11-09 22:29:06 +0000

    corrections to the tangle

--- src/arr_xml/core.clj
+++ src/arr_xml/core.clj
@@ -3,7 +3,12 @@
 
 (use 'conduit.core 'arrows.core 'arrows-extra.core 'clojure.data.xml)
 
-
+(defn arr-switch-elem [default-channel name channel]
+ (arr-toggle-switch-inclusive default-channel (fn[input] (and (= (:name input) name) (= (:type input) :start-element)))
+                                           (fn[input] (and (= (:name input) name) (= (:type input) :end-element)))
+                                           channel
+ )
+)
 (defn arr-select-elem [name]
    (a-comp (arr-switch-elem name pass-through) block)
   )
@@ -21,3 +26,10 @@
     (if chars [chars] [])
     )
   )
+
+
+(def-arr arr-stream-to-xml-seq [instream]
+  ":: InputStream -> [XML]"
+  (source-seq instream :coalescing false)
+
+)
